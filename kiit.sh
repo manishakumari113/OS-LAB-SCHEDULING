71847d36fff841ddf98444adc6d1bbb6c38d7832
manishakumari113 <134261050+manishakumari113@users.noreply.github.com>
2023-06-02 13:34:56 +0530

    Add files via upload

---
+++ kiit.sh
@@ -0,0 +1,13 @@
+echo "enter two no.:"
+read A
+read B
+c=`expr $A + $B`
+echo sum:$c
+d=`expr $A - $B`
+echo difference:$d
+e=`expr $A \* $B`
+echo product:$e
+f=`expr $A / $B`
+echo quoitent:$f
+
+
